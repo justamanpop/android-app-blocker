2a3dcef3dc97e6ef78c885acf3d32215a65b2b97
anish <pogchampoo24@gmail.com>
2026-07-27 23:12:37 +0200

    add create block set functionality

---
+++ app/src/main/java/com/example/appblocker/ui/screens/addBlockSetScreen/AddBlockSetScreen.kt
@@ -0,0 +1,33 @@
+package com.example.appblocker.ui.screens.addBlockSetScreen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AddBlockSetScreen(viewModel: AddBlockSetScreenViewModel) {
+    var nameTextFieldValue by remember { mutableStateOf("") }
+    Scaffold() {
+        innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            OutlinedTextField(nameTextFieldValue, {
+                nameTextFieldValue = it
+            })
+            Button({
+                viewModel.createBlockSet(nameTextFieldValue)
+            }) {
+                Text("Create block set")
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/appblocker/ui/screens/addBlockSetScreen/AddBlockSetScreenViewModel.kt
+++ app/src/main/java/com/example/appblocker/ui/screens/addBlockSetScreen/AddBlockSetScreenViewModel.kt
@@ -5,11 +5,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
 import com.example.appblocker.AppBlockSetPreferences
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
-class AddBlockSetScreenViewModelFactory(private val dataStore: DataStore<List<AppBlockSetPreferences>>) : ViewModelProvider.Factory {
+class AddBlockSetScreenViewModelFactory(private val dataStore: DataStore<List<AppBlockSetPreferences>>) :
+    ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(AddBlockSetScreenViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
@@ -19,5 +18,14 @@ class AddBlockSetScreenViewModelFactory(private val dataStore: DataStore<List<Ap
     }
 }
 
-class AddBlockSetScreenViewModel(val dataStore: DataStore<List<AppBlockSetPreferences>>) : ViewModel() {
+class AddBlockSetScreenViewModel(val dataStore: DataStore<List<AppBlockSetPreferences>>) :
+    ViewModel() {
+    fun createBlockSet(blockSetName: String) {
+        viewModelScope.launch {
+            dataStore.updateData { curr ->
+                val maxId = curr.maxByOrNull { it.id }?.id ?: 1
+                curr + AppBlockSetPreferences(maxId + 1, blockSetName, listOf())
+            }
+        }
+    }
 }
